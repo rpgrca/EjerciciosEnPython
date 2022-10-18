bc806fb847d7b87559bbc8d98d291f780dba8afe
Roberto <rpgrca@gmail.com>
2022-10-18 13:46:24 -0300

    add test, over 5 minutes round up and 150 cent per minute

--- FacturacionDeLlamadas/main.py
+++ FacturacionDeLlamadas/main.py
@@ -3,8 +3,11 @@ from ddt import ddt, data
 
 class Facturadora:
     def __init__(self, minutos, segundos):
-        self.__total = (minutos * 60 + segundos) * 3
-        
+        if (minutos < 5):
+            self.__total = (minutos * 60 + segundos) * 3
+        else:
+            self.__total = minutos * 150
+
     def obtenerTotal(self):
         return self.__total
 
@@ -15,7 +18,10 @@ class FacturacionDeLlamadasUnitTests(unittest.TestCase):
     def test_si_llamada_dura_menos_de_5_minutos_entonces_se_cobra_3_centavos_por_segundo(self, values):
         sut = Facturadora(values[0], values[1])
         self.assertEqual(values[2], sut.obtenerTotal())
-    
-    
+
+    def test_si_llamada_mayor_5_minutos_entonces_redondear_segundos_al_minuto_superior_y_cobrar_150_centavos_por_minuto(self):
+        sut = Facturadora(6, 0)
+        self.assertEqual(900, sut.obtenerTotal())
+
 if __name__ == "__main__":
     unittest.main()
