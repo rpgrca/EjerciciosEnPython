d8a6046c4cbc10bc677988482227f33400f9634c
Roberto <rpgrca@gmail.com>
2022-10-18 13:43:09 -0300

    modify test, accept minutes and seconds

--- FacturacionDeLlamadas/main.py
+++ FacturacionDeLlamadas/main.py
@@ -1,16 +1,20 @@
 import unittest
+from ddt import ddt, data
 
 class Facturadora:
-    def __init__(self, duracion):
-        self.__total = duracion * 3
+    def __init__(self, minutos, segundos):
+        self.__total = (minutos * 60 + segundos) * 3
         
     def obtenerTotal(self):
         return self.__total
 
+@ddt
 class FacturacionDeLlamadasUnitTests(unittest.TestCase):
-    def test_si_llamada_dura_menos_de_5_minutos_entonces_se_cobra_3_centavos_por_segundo(self):
-        sut = Facturadora(3)
-        self.assertEqual(9, sut.obtenerTotal())
+    
+    @data((0, 0, 0), (3, 30, 630))
+    def test_si_llamada_dura_menos_de_5_minutos_entonces_se_cobra_3_centavos_por_segundo(self, values):
+        sut = Facturadora(values[0], values[1])
+        self.assertEqual(values[2], sut.obtenerTotal())
     
     
 if __name__ == "__main__":
